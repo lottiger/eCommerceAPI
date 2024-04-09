fe24986d7423d8e065d7251aaba7c7003d0be624
lottiger <linda.lottiger@gmail.com>
2024-04-09 10:15:49 +0200

    clean up

--- models/productModel.js
+++ models/productModel.js
@@ -29,36 +29,3 @@ const Product = model('Product', productSchema)
 export default Product
 
 
-
-
-
-
-// import {Schema, model} from 'mongoose'
-
-// const productSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: [true, 'Please enter product name']
-//     },
-//     price: {
-//         type: Number,
-//         required: [true, 'Please enter product price']
-//     },
-//     description: {
-//         type: String,
-//         required: [true, 'Please enter product description']
-//     },
-//     category: {
-//         type: String,
-//         required: [true, 'Please enter product category']
-//     },
-    
-//     imageUrl: {
-//         type: String,
-//         required: [true, 'Please enter product image URL']
-//     }
-// }, {timestamps: true})
-
-// const Product = model('Product', productSchema)
-
-// export default Product
